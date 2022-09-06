f8902772ecbfd7cdf5737a885eb6364371ade864
worldmozara <worldmozara@gmail.com>
2022-09-06 14:01:12 +0800

    use threading.

--- qute/msgsvr/gocqhttp.py
+++ qute/msgsvr/gocqhttp.py
@@ -3,6 +3,10 @@ from threading import Thread
 
 app = Flask(__name__)
 
-thrd = Thread(target=app.run)
 
-thrd.start()
+def start(port: int):
+    global thrd
+    thrd = Thread(
+        target=app.run, kwargs={"host": "0.0.0.0", "port": port}, daemon=True
+    )
+    thrd.start()
