084ebc7d191621eda90f5b730127241d1af68e43
worldmozara <worldmozara@gmail.com>
2022-09-06 15:55:04 +0800

    add simple receiver.

--- qute/msgsvr/gocqhttp.py
+++ qute/msgsvr/gocqhttp.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from threading import Thread
+import logging
 
 app = Flask(__name__)
+logging.getLogger('werkzeug').disabled = True
+
+
+@app.route("/", methods=["POST"])
+def receive():
+    data = request.json
+    if not data:
+        return "Bad event", 400
+    if (data["post_type"] == "meta_event" and
+            data["meta_event_type"] == "heartbeat"):
+        return "OK", 200
+    print(data)
+    return "OK", 200
 
 
 def start(port: int):
